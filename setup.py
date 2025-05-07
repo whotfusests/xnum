3f44a2eb420cecff0331cd3fb4c9f5945c7d89a3
whotfusests <hamzakorkmaz9193@gmail.com>
2025-05-07 17:27:58 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="xnum",
+    version="2.0",
+    author="whotfusests",
+    description="Encryption module for Python with /x64/ like encrypt lines and complete gibberish encryption.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/whotfusests/xnum",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[],
+    extras_require={
+        'dev': [
+            'pytest>=6.0',
+            'twine>=3.0',
+        ],
+    },
+)
