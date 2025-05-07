ce051738f7b0040cf63eb8b7d27da8dfbb3c02f9
whotfusests <hamzakorkmaz9193@gmail.com>
2025-05-08 01:07:25 +0300

    Add files via upload

---
+++ xnum/__init__.py
@@ -0,0 +1,11 @@
+from .xnum import (
+    mode,
+    genkey,
+    encrypt,
+    decrypt,
+    version,
+    credits
+)
+
+__version__ = "4.0"
+__all__ = ['mode', 'genkey', 'encrypt', 'decrypt', 'version', 'credits']
